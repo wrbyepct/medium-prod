861f0d38a37cf7f8318e5dec957dd070ba0ec793
Furthermore <wrbyepcr11@gmail.com>
2024-12-24 11:02:20 +0800

    feat: add Response admin

--- core/apps/responses/admin.py
+++ core/apps/responses/admin.py
@@ -1,1 +1,19 @@
 """Response Adim."""
+
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.Response)
+class ResponseAdmin(admin.ModelAdmin):
+    """Response admin."""
+
+    list_display = ["id", "description", "created_at", "updated_at"]
+    list_display_links = ["id", "description"]
+    list_filter = ["user__first_name", "user__last_name", "created_at", "updated_at"]
+    search_fields = ["article__title"]
+
+    def description(self, obj: models.Response):
+        """Return instance string name."""
+        return obj.__str__()
