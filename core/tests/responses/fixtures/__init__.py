8d9715955efbb9a6074fdd4fadbc97fd25c61d9a
Furthermore <wrbyepcr11@gmail.com>
2025-02-09 02:14:02 +0800

    test: Add childe reply e2e list create endpoint tests

--- core/tests/responses/fixtures/__init__.py
+++ core/tests/responses/fixtures/__init__.py
@@ -1,6 +1,12 @@
+import pytest
 from pytest_factoryboy import register
 
 from .factories import ResponseClapFactory, ResponseFactory
 
 register(ResponseFactory)
 register(ResponseClapFactory)
+
+
+@pytest.fixture
+def response(response_factory):
+    return response_factory.create()
