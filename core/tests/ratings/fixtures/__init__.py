f3cb7ae0943d1d8bfbfcdbbbfc20c9f7005b89c0
Furthermore <wrbyepcr11@gmail.com>
2025-02-12 21:08:40 +0800

    test: Add rating update delete e2e tests

--- core/tests/ratings/fixtures/__init__.py
+++ core/tests/ratings/fixtures/__init__.py
@@ -1,5 +1,11 @@
+import pytest
 from pytest_factoryboy import register
 
 from .factories import RatingFactory
 
 register(RatingFactory)
+
+
+@pytest.fixture
+def rating(rating_factory, normal_user):
+    return rating_factory.create(user=normal_user)
