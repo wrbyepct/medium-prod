af94aae44d976014f47d278a81921ede330b6d69
Furthermore <wrbyepcr11@gmail.com>
2025-05-11 02:09:39 +0800

    fix: turn wait_for_db script into django command, remove entrypoint file for api dockerfile

---
+++ core/apps/general/management/commands/wait_for_db.py
@@ -0,0 +1,33 @@
+"""Command for probing DB."""
+
+# ruff: noqa: ARG002
+import sys
+import time
+
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg2OpError
+
+
+class Command(BaseCommand):
+    """Django command to wait for the database to be available."""
+
+    def handle(self, *args, **options):
+        """Probe for PostGres avaiability, after 30 secs report error."""
+        self.stdout.write("Waiting for PostgreSQL to become available...")
+        suggest_unrecoverable_after = 30
+        start = time.time()
+        while True:
+            try:
+                conn = connections["default"]
+                conn.cursor()
+                break
+            except (Psycopg2OpError, OperationalError) as e:
+                sys.stderr.write("Waiting for PostgreSQL to become available...\n")
+                if time.time() - start > suggest_unrecoverable_after:
+                    sys.stderr.write(
+                        f"This is taking longer than expected, the following exception may be indicative of an unrecoverable error: '{e}'\n"
+                    )
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("PostgreSQL is available."))
