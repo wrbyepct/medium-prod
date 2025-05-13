5423933265c4992cab044912616c031c8ae87fab
Furthermore <wrbyepcr11@gmail.com>
2025-05-13 23:02:58 +0800

    fix: ses egress rule not set

--- terraform/deploy/ecs.tf
+++ terraform/deploy/ecs.tf
@@ -405,6 +405,14 @@ resource "aws_security_group" "ecs_service" {
     ]
   }
 
+  # For ECS t connect to SES
+  egress {
+    from_port   = 587
+    to_port     = 587
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # For ECS to access vpc endpoints
   egress {
     from_port   = 443
