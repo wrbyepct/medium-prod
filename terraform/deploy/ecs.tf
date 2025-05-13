60a20618352508cedc4c21fe8f905b477e6bb617
Furthermore <wrbyepcr11@gmail.com>
2025-05-13 17:48:56 +0800

    fix: add ecs egress rule for elasticache

--- terraform/deploy/ecs.tf
+++ terraform/deploy/ecs.tf
@@ -395,6 +395,17 @@ resource "aws_security_group" "ecs_service" {
       aws_subnet.private[1].cidr_block
     ]
   }
+  
+  # Egress for ElastiCache
+  egress {
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [
+      aws_subnet.private[0].cidr_block,
+      aws_subnet.private[1].cidr_block
+    ]
+  }
 
   # For ECS to access vpc endpoints
   egress {
