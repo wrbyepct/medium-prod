988f60c30338df478f2ed14217dce8b679294a45
Furthermore <wrbyepcr11@gmail.com>
2025-05-16 16:35:32 +0800

    fix: comment out dehub tool setting

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+# ruff: noqa: ERA001
 """Project level urls."""
 
 from dj_rest_auth.views import PasswordResetConfirmView
@@ -51,7 +52,7 @@ urlpatterns += [
 ]
 
 
-if settings.DEBUG and not settings.IN_TEST:
-    from debug_toolbar.toolbar import debug_toolbar_urls
+# if settings.DEBUG and not settings.IN_TEST:
+#     from debug_toolbar.toolbar import debug_toolbar_urls
 
-    urlpatterns = urlpatterns + debug_toolbar_urls()
+#     urlpatterns = urlpatterns + debug_toolbar_urls()
